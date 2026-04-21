7d6a8ae464c918e6401c61ab7925bfce945ab5e5
SainiAryan11 <sainiaryan2020@gmail.com>
2026-04-21 13:31:47 +0530

    Add files via upload

---
+++ FSD2/express/lec5/server.js
@@ -0,0 +1,15 @@
+express = require('express');
+app = express();
+cp = require('cookie-parser');
+
+app.use(cp());
+
+app.get('/',(req,res)=>{
+    res.cookie('fname','abc');
+    res.cookie('lname','pqr');
+    res.cookie('email','abc@gmail.com');
+    res.cookie('contact','979797');
+    res.send('cookie stored');
+});
+
+app.listen(5678);
---
+++ FSD2/express/lec5/server2.js
@@ -0,0 +1,16 @@
+express = require('express');
+app = express();
+cp = require('cookie-parser');
+
+app.use(cp());
+
+app.get('/',(req,res)=>{
+    res.cookie('fname','abc');
+    res.cookie('lname','pqr');
+    res.cookie('email','abc@gmail.com',{maxAge:10000});
+    res.cookie('contact','979797');
+    // res.clearCookie('contact');
+    res.send('cookie stored');
+});
+
+app.listen(5678);
